c403e4338d7ba8b5f521101c81c442e2182e5640
Mali Yassine <yassine.mali20@ump.ac.ma>
2023-04-02 18:25:42 +0000

    Ajouter template du client

--- Fallah_App/Migrations/20230402051954_Creation.Designer.cs
+++ Fallah_App/Migrations/20230402051954_Creation.Designer.cs
@@ -4,6 +4,7 @@ using Falla7_App.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fallah_App.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20230402051954_Creation")]
+    partial class Creation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
