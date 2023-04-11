518343f20a012994867c47d5324de26c21949423
Mali Yassine <yassine.mali20@ump.ac.ma>
2023-04-11 14:54:54 +0000

    deamdes fini il reste evoie mail

--- Fallah_App/Controllers/WebMaster/DemandesController.cs
+++ Fallah_App/Controllers/WebMaster/DemandesController.cs
@@ -2,6 +2,7 @@
 using Fallah_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using System.Net.Mail;
 
 namespace Fallah_App.Controllers.WebMaster
 {
@@ -19,20 +20,38 @@ namespace Fallah_App.Controllers.WebMaster
         public IActionResult List()
         {
             RemplireCache();
-            // return View(this.memoryCache.Get<List<Demande>>("Demandes"));
-            return View(db.demandes.ToList());
+            //EnvoyerEmailAcceptation();
+            return View(this.memoryCache.Get<List<Demande>>("Demandes"));
+            //return View(db.demandes.ToList());
         }
         public IActionResult Information(int id)
-        {
+        {  
+            if(id==null || !(id is int))
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
             RemplireCache();
             Demande demande = this.memoryCache.Get<List<Demande>>("Demandes").Where(demande => demande.Id == id).FirstOrDefault();
+            if (demande == null)
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
             return View(demande);
         }
 
         public IActionResult Accepter(int id)
         {
-            Demande demande = db.demandes.Find(id);
-            if(demande.forme==true)
+            if (id == null || !(id is int))
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
+            RemplireCache();
+            Demande demande = this.memoryCache.Get<List<Demande>>("Demandes").Where(demande => demande.Id == id).FirstOrDefault();
+            if (demande == null)
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
+            if (demande.forme==true)
             {
                 AgriculteurForme agriculteurForme = new AgriculteurForme();
                 agriculteurForme.Nom = demande.Nom;
@@ -62,25 +81,58 @@ namespace Fallah_App.Controllers.WebMaster
 
             db.demandes.Remove(demande);
             db.SaveChanges();
+            memoryCache.Remove("Demandes");
+            RemplireCache();
+            
             return RedirectToAction("List");
         }
 
         public IActionResult Refuser(int id)
         {
+            if (id == null || !(id is int))
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
+            RemplireCache();
             Demande demande = db.demandes.Find(id);
+            if (demande == null)
+            {
+                return RedirectToAction("Index", "ERROR404");
+            }
             demande.Statut = true;
             //demande.Id_WebMaster = (int)HttpContext.Session.GetInt32("webMasterID");
             db.demandes.Update(demande);
             db.SaveChanges();
+            memoryCache.Remove("Demandes");
+            RemplireCache();
             return RedirectToAction("List");
         }
 
         public void RemplireCache()
         {
-            if (this.memoryCache.Get<List<Demande>>("Demandes") == null)
+            List<Demande> demandes = null;
+            if (!this.memoryCache.TryGetValue("Demandes" ,out demandes))
             {
                 this.memoryCache.Set("Demandes", db.demandes.ToList(), TimeSpan.FromHours(2));
             }
+        }
+        public void EnvoyerEmailAcceptation()
+        {
+            string from = "falla7app@gmail.com";
+            string subject = "Acceptation de votre demande de compte";
+            string body = "Je suis ravi(e) de vous informer que votre demande de compte a été acceptée avec succès. " +
+                "Nous sommes heureux de vous accueillir en tant que nouveau client chez FallahApp." +
+                "Votre nouveau compte est maintenant actif et vous pouvez accéder à toutes les fonctionnalités et services proposés par notre entreprise. " +
+                "Nous sommes convaincus que vous apprécierez la qualité de nos produits et services. " +
+                "N'hésitez pas à nous contacter si vous avez des questions ou si vous avez besoin d'aide pour utiliser votre compte. Nous sommes là pour vous aider à tout moment." +
+                "Nous vous remercions de votre confiance et espérons vous offrir une expérience exceptionnelle chez FallahApp." +
+                "Cordialement,";
+
+            MailMessage message = new MailMessage(from, "yassinemali300@gmail.com", subject, body);
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential("falla7app@gmail.com", "yxlodflzpcvnvjvh");
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(message);
         }
     }
 }
