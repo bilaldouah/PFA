f8cff5e3985234a94d0fd4addb2bc70b674cc946
bilaldouah <douah.bilaal@gmail.com>
2023-04-16 15:33:39 +0000

    ajouter des error de form

--- Fallah_App/Models/Plante.cs
+++ Fallah_App/Models/Plante.cs
@@ -1,14 +1,27 @@
-﻿namespace Fallah_App.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Fallah_App.Models
 {
     public class Plante
-    {
-        public int Id { get; set; }
+    {        
+        public int ?  Id { get; set; }
+        [Required (ErrorMessage="ce champ est obligatoire")]
         public string Nom { get; set; }
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         public string type { get; set; }
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         public DateTime Debut_Period { get; set; }
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         public DateTime Fin_Date { get; set; }
-        public List<Notification> notifications { get; set; }
-        public List<Terre> terres { get; set; }
-        public List<ConseilPlante> conseilPlantes { get; set; }
+       
+        public List<Notification> ? notifications { get; set; }
+        
+        public List<Terre> ? terres { get; set; }
+         
+        public List<ConseilPlante> ? conseilPlantes { get; set; }
+        
     }
 }
