e85a0e3d9ffcb7dbfa4ee5ce2bfc107d7c6762ff
Adnane <khabbouriadnane198@gmail.com>
2023-04-18 21:43:40 +0000

    ajouter confirmation du password

--- Fallah_App/Models/Demande.cs
+++ Fallah_App/Models/Demande.cs
@@ -7,32 +7,35 @@ namespace Fallah_App.Models
     public class Demande
     {    
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [StringLength(100)]
         public string Nom { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [StringLength(100)]
         public string Prenom { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [RegularExpression(@"^(\+212|0)[5-7]\d{8}$", ErrorMessage = "Invalid phone number format.")]
         public string Telephone { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [DataType(DataType.Date)]
         public DateTime Date_De_Naissance { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [StringLength(100)]
         public string Login { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [StringLength(100)]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
+        [StringLength(100)]
+        [NotMapped]
+        public string conf_Password { get; set; }
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         [EmailAddress( ErrorMessage = "Invalid Email format.")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         public DateTime Date_Demande{ get; set; }
-        [Required]
+        [Required(ErrorMessage = "ce champ est obligatoire")]
         public string Image { get; set; }
-        [Required]
         public Boolean Statut { get; set; }
         public Boolean forme { get; set; }
         public WebMaster? webMaster { get; set; }
