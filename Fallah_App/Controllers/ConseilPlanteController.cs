f61089d863305c38c8f8fe7d5e7fb6d0006da1d4
Zekhnini anass <anasszekhnini682@gmail.com>
2023-05-17 01:23:01 +0200

    commite ajouter conseilplante

---
+++ Fallah_App/Controllers/ConseilPlanteController.cs
@@ -0,0 +1,36 @@
+﻿using Fallah_App.Context;
+using Fallah_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Fallah_App.Controllers
+{
+    public class ConseilPlante : Controller
+    {
+        MyContext db;
+        public ConseilPlante(MyContext db)
+        {
+           
+            this.db = db;
+        }
+        public IActionResult List() 
+        {
+            return View(db.conseilPlantes.ToList());
+                
+                }
+        public IActionResult Ajouter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ajouter(Models.ConseilPlante csp)
+        {
+           
+            db.conseilPlantes.Add(csp);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+    }
+}
